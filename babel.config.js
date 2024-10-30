4f45fa7164f9f3cd72705d8b2802ef2fa6b2a7be
Bluette1 <marylene.sawyer@gmail.com>
2024-10-30 22:46:45 +0200

    chore: Prep for Deployment
    - Prepare to deploy app
    
    Issue #3

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
+  const plugins = [];
+
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['transform-inline-environment-variables'],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+
+    plugins,
   };
 };
