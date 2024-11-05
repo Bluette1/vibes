a2e5cfd354293524e205f31ae48f43ddb9c89f8c
Bluette1 <marylene.sawyer@gmail.com>
2024-11-05 05:29:55 +0200

    fix: Breaking Test
    - Fix breaking test
    - Add test for offline mode
    
    Issue #5

---
+++ __tests___/__mocks__/@react-native-community/netinfo.ts
@@ -0,0 +1,14 @@
+// __mocks__/@react-native-community/netinfo.ts
+export default {
+    addEventListener: jest.fn(),
+    fetch: jest.fn().mockImplementation(() => Promise.resolve({
+      isConnected: true,
+      isInternetReachable: true,
+      type: 'wifi'
+    })),
+    useNetInfo: jest.fn().mockImplementation(() => ({
+      isConnected: true,
+      isInternetReachable: true,
+      type: 'wifi'
+    }))
+  };
